f0b737759f5a7eeaf3de0cbee5bc39a9f50f70f9
lujinyi <15071437004@139.com>
2017-11-08 22:45:39 +0800

    complete first homework

---
+++ lujinyi/pages/home/home.js
@@ -0,0 +1,17 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    array:[{message:'a'},
+      { message: 'b' },
+      { message: 'c' },
+      { message: 'd' },
+      { message: 'e' },
+      { message: 'f' },
+      { message: 'g' }
+    ]
+  }
+})
