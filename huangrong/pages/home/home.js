c590644845128eab781bcdc80346ec304c8e98a8
hilree <hilree@outlook.com>
2017-11-09 14:04:36 +0800

    finish function 'add' and 'sub' and add new flag

--- huangrong/pages/home/home.js
+++ huangrong/pages/home/home.js
@@ -1,5 +1,7 @@
 Page({
   data: {
+    canAdd: true,
+    canSub:false,
     Array: ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
     ArrayBuff: ['h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
   },
@@ -7,14 +9,18 @@ Page({
     const length = this.data.Array.length
     this.data.Array.push(this.data.ArrayBuff[length - 7])
     this.setData({
-      Array: this.data.Array
+      Array: this.data.Array,
+      canAdd: length < 25,
+      canSub: length > 5
     })
   },
   sub: function (e) {
     const length = this.data.Array.length
     this.data.Array.pop()
     this.setData({
-      Array: this.data.Array
+      Array: this.data.Array,
+      canAdd: length < 25,
+      canSub: length > 8
     })
   }
 })
