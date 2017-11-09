e43e594805fbd356375fd8dec575d8e10dea6835
dragonhaw <dragonhaw@163.com>
2017-11-09 13:51:28 +0800

    complete add and delete

--- hejialong/pages/home/home.js
+++ hejialong/pages/home/home.js
@@ -1,10 +1,53 @@
 // pages/home/home.js
+var texts = [
+  "8 'h'",
+  "9 'i'",
+  "10 'j'",
+  "11 'k'",
+  "12 'l'",
+  "13 'm'",
+  "14 'n'",
+  "15 'o'",
+  "16 'p'",
+  "17 'q'",
+  "18 'r'",
+  "19 's'",
+  "20 't'",
+  "21 'u'",
+  "22 'v'",
+  "23 'w'",
+  "24 'x'",
+  "25 'y'",
+  "26 'z'"
+];
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    arr: ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    arr: ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
+    text: '',
+    canAdd: true,
+    canRemove: false
+  },
+  extraLine: [],
+  add: function (e) {
+    this.extraLine.push(texts[this.extraLine.length % 19])
+    this.setData({
+      text: this.extraLine.join('\n'),
+      canAdd: this.extraLine.length < 19,
+      canRemove: this.extraLine.length > 0
+    })
+  },
+  delete: function (e) {
+    if (this.extraLine.length > 0) {
+      this.extraLine.pop()
+      this.setData({
+        text: this.extraLine.join('\n'),
+        canAdd: this.extraLine.length < 19,
+        canRemove: this.extraLine.length > 0
+      })
+    }
   }
 })
