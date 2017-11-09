492af2cb0aad2a740bc3aa04f0a3abd6f3094e6b
tkox <1945483930@qq.com>
2017-11-09 14:03:50 +0800

    finish bottons add && remove

--- xuxuegang/pages/home/home.js
+++ xuxuegang/pages/home/home.js
@@ -1,13 +1,35 @@
-const app = getApp()
-
 Page({
   data: {
-     array: [{message : 'a'}, 
-     {message : 'b'},
-     {message : 'c'},
-     {message : 'd'},
-     {message : 'e'},
-     {message : 'f'},
-     {message : 'g'},]
+    arr: ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
+    alp:['h', 'i', 'j' , 'k' , 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't',
+    'u', 'v', 'w', 'x', 'y', 'z'],
+    arrIsFull: false,
+    alpIsFull: true
+  },
+  add: function(e) {
+    if(this.data.arr.length < 26){
+      var alpha = this.data.alp.shift()
+      this.data.arr.push(alpha)
+      this.setData({
+        alpIsFull: false
+      })
+    }
+    else this.setData({
+      arrIsFull: true
+    })
+
+  },
+  remove: function(e) {
+    if(this.data.arr.length > 7){
+      var alpha = this.data.arr.pop()
+      this.data.alp.unshift(alpha)
+      this.setData({
+        arrIsFull: false
+      })
+    }
+    else this.setData({
+      alpIsFull: true
+    })
   }
 })
