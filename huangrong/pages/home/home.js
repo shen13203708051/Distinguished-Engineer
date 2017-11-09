50dc2f2a9b3f29f2d9828f1603ecaa710b68fecf
hilree <hilree@outlook.com>
2017-11-09 13:52:01 +0800

    Add function 'add' and 'sub'

--- huangrong/pages/home/home.js
+++ huangrong/pages/home/home.js
@@ -1,6 +1,20 @@
 Page({
   data: {
-    array: ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
-    arraynew:[]
+    Array: ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
+    ArrayBuff: ['h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  },
+  add: function (e) {
+    const length = this.data.Array.length
+    this.data.Array.push(this.data.ArrayBuff[length - 7])
+    this.setData({
+      Array: this.data.Array
+    })
+  },
+  sub: function (e) {
+    const length = this.data.Array.length
+    this.data.Array.pop()
+    this.setData({
+      Array: this.data.Array
+    })
   }
 })
