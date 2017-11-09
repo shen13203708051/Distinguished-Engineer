fcae3898fb436d4c7f0a97375986df83ff23e984
lujinyi <15071437004@139.com>
2017-11-09 14:28:24 +0800

    update function add and funtion minus

--- lujinyi/pages/home/home.js
+++ lujinyi/pages/home/home.js
@@ -4,22 +4,25 @@ Page({
   /**
    * 页面的初始数据
    */
-  arraynow:['a', 'b', 'c', 'd', 'e' , 'f'],
+  arrayNow:['a', 'b', 'c', 'd', 'e' , 'f', 'g'],
   arrayTemp:['a', 'b', 'c', 'd', 'e' , 'f' , 'g', 'h', 'i', 'j', 'k', 'l', 'm',
   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
   data: {
     array:['a', 'b', 'c', 'd', 'e' , 'f', 'g'],
-    disabled1: true,
-    disabled2:false
+    disabled1: false,
+    disabled2: false
   },
 
 add: function(e){
-
+this.arrayNow.push(this.arrayTemp[this.arrayNow.length])
+this.setData({
+array:this.arrayNow
+})
 },
 minus: function(e){
-this.arraynow.pop()
+this.arrayNow.pop()
 this.setData({
-array:this.arraynow
+array:this.arrayNow
 })
 }
 })
