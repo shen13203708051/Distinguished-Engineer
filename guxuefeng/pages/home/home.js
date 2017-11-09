05acb3fdc0cf9b47f2924dc840f070cc07ef99e9
guxuefeng <gxf41x@163.com>
2017-11-09 16:30:37 +0800

    finished the button

--- guxuefeng/pages/home/home.js
+++ guxuefeng/pages/home/home.js
@@ -1,5 +1,27 @@
+var extraLine = [];
 Page({
     data: {
-        array: ['a','b','c','d','e','e','f']
-      }
+        array: ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
+        extraArray: ['h', 'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z'
+        ],
+    }
 })
+
+
+
+
+//         this.setData({
+//           array :
+//       
+//     })
+// }
+//   },
+//   minus: function(e) {
+//     if (array.length > 0) {
+//       array.pop()
+//       this.setData({
+//        
+//       })
+//     }
