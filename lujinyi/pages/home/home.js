efb21e148311bfbcc9b6ec539d312cd23f3bb60d
lujinyi <15071437004@139.com>
2017-11-09 13:06:32 +0800

    add an array templete

--- lujinyi/pages/home/home.js
+++ lujinyi/pages/home/home.js
@@ -5,13 +5,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-    array:['a',
-       'b',
-        'c' ,
-        'd' ,
-       'e' ,
-        'f' ,
-        'g' 
-    ]
-  }
+    array:['a', 'b', 'c', 'd', 'e' , 'f' , 'g', 'h'],
+    arrayTemp:['a', 'b', 'c', 'd', 'e' , 'f' , 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    disabled1: true,
+    disabled2:false
+  },
+
+add:function(e){
+
+},
+minus:function(e){
+
+}
 })
