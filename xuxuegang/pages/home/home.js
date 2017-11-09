8c6d2f4af5b45edf69fb8bad92d6fba97dc58e36
tkox <1945483930@qq.com>
2017-11-09 14:14:33 +0800

    finish event function : add && remove

--- xuxuegang/pages/home/home.js
+++ xuxuegang/pages/home/home.js
@@ -4,32 +4,25 @@ Page({
     alp:['h', 'i', 'j' , 'k' , 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't',
     'u', 'v', 'w', 'x', 'y', 'z'],
-    arrIsFull: false,
-    alpIsFull: true
   },
   add: function(e) {
-    if(this.data.arr.length < 26){
-      var alpha = this.data.alp.shift()
-      this.data.arr.push(alpha)
-      this.setData({
-        alpIsFull: false
-      })
-    }
-    else this.setData({
-      arrIsFull: true
+    let newArr = this.data.arr;
+    let newAlp = this.data.alp;
+    let alpha = newAlp.shift();
+    newArr.push(alpha)
+    this.setData({
+      arr: newArr,
+      alp: newAlp
     })
-
   },
   remove: function(e) {
-    if(this.data.arr.length > 7){
-      var alpha = this.data.arr.pop()
-      this.data.alp.unshift(alpha)
-      this.setData({
-        arrIsFull: false
-      })
-    }
-    else this.setData({
-      alpIsFull: true
+    let newArr = this.data.arr;
+    let newAlp = this.data.alp;
+    let alpha = newArr.pop()
+    newAlp.unshift(alpha)
+    this.setData({
+      arr: newArr,
+      alp: newAlp
     })
   }
 })
